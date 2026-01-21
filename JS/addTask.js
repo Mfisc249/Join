4a8d49398c0cd86bef79c226fd3ee0754f6adff0
60eren <2010erenim@gmail.com>
2026-01-21 19:25:29 +0100

    Merge conflict resolved in add_task.html

---
+++ JS/addTask.js
@@ -0,0 +1,183 @@
+const BASE_URL =
+  "https://join-6f9cc-default-rtdb.europe-west1.firebasedatabase.app/";
+
+let taskCounter = 1;
+
+const urgentBtn = document.querySelector(".importanceLevel:nth-child(1)");
+const mediumBtn = document.querySelector(".importanceLevel:nth-child(2)");
+const lowBtn = document.querySelector(".importanceLevel:nth-child(3)");
+
+let task = {
+  title: "",
+  description: "",
+  dueDate: "",
+  priority: "",
+  assignedTo: [],
+  category: "",
+  subtasks: [],
+};
+
+async function init() {
+  renderTemplate();
+  setupPriorityButtons();
+  setDefaultPriority();
+  setDateMin();
+  await loadTaskCounter();
+}
+
+function renderTemplate() {
+  document.getElementById("mainContent").innerHTML = createTaskTemplate();
+}
+
+async function saveTask(task) {
+  const taskKey = `addTask${taskCounter}`;
+
+  const response = await fetch(`${BASE_URL}addTask/${taskKey}.json`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(task),
+  });
+
+  if (response.ok) {
+    console.log("Task gespeichert als:", taskKey);
+    taskCounter++; // nächster Task
+  }
+}
+
+async function loadTaskCounter() {
+  const response = await fetch(`${BASE_URL}tasks.json`);
+  const data = await response.json();
+
+  if (!data) {
+    taskCounter = 1;
+  } else {
+    taskCounter = Object.keys(data).length + 1;
+  }
+}
+
+async function createTask() {
+  const titleInput = document.getElementById("taskName");
+  const descInput = document.getElementById("taskDesc");
+  const subtaskInput = document.getElementById("subtaskInput");
+  const assignedInput = document.getElementById("assignedTo");
+  const categoryInput = document.getElementById("category");
+  const dateInput = document.getElementById("DueDate");
+
+  task.assignedTo = assignedInput.value;
+  task.category = categoryInput.value;
+  task.title = titleInput.value;
+  task.description = descInput.value;
+  task.dueDate = dateInput.value;
+  task.createdAt = Date.now();
+
+  if (subtaskInput.value) {
+    task.subtasks = subtaskInput.value.split(",");
+  } else {
+    task.subtasks = [];
+  }
+  errorMessage();
+  if (
+    !task.title ||
+    !task.description ||
+    !task.dueDate ||
+    !task.priority ||
+    !task.category
+  ) {
+  }
+
+  await saveTask(task);
+  clearForm();
+}
+
+function errorMessage() {
+  toggleRequired(document.getElementById("taskName"));
+  toggleRequired(document.getElementById("taskDesc"));
+  toggleRequired(document.getElementById("DueDate"));
+  toggleRequired(document.getElementById("priority"));
+}
+function clearForm() {
+  document.getElementById("taskForm").reset();
+  task.title = "";
+  task.description = "";
+  task.dueDate = "";
+  task.priority = "";
+  task.assignedTo = [];
+  task.category = "";
+  task.subtasks = [];
+  task.createdAt = null;
+
+  const subtaskInput = document.getElementById("subtaskInput");
+  if (subtaskInput) subtaskInput.value = "";
+
+  console.log("Formular + Task-Objekt + Subtasks zurückgesetzt ✅");
+}
+
+function toggleRequired(inputElement) {
+  if (!inputElement) return;
+
+  const container = inputElement.closest("label") || inputElement.parentElement;
+  const requiredText = container.querySelector(".requiredField");
+
+  if (!requiredText) return;
+
+  if (!inputElement.value) {
+    requiredText.classList.add("visible");
+  } else {
+    requiredText.classList.remove("visible");
+  }
+}
+
+function setupPriorityButtons() {
+  const buttons = document.querySelectorAll(".importanceLevel");
+
+  buttons.forEach((btn) => {
+    btn.onclick = () => {
+      if (btn.classList.contains("active")) {
+        // Button deaktivieren
+        btn.classList.remove("active");
+        btn.style.backgroundColor = "white";
+        btn.style.color = "black";
+        task.priority = "";
+      } else {
+        // Alle Buttons zurücksetzen
+        buttons.forEach((b) => {
+          b.classList.remove("active");
+          b.style.backgroundColor = "white";
+          b.style.color = "black";
+        });
+
+        // Geklickten Button aktivieren
+        btn.classList.add("active");
+        task.priority = btn.textContent;
+
+        if (btn.textContent === "Urgent") btn.style.backgroundColor = "#ff2020";
+        if (btn.textContent === "Medium") btn.style.backgroundColor = "orange";
+        if (btn.textContent === "Low") btn.style.backgroundColor = "#00cf00";
+
+        btn.style.color = "white";
+      }
+    };
+  });
+}
+
+function setDefaultPriority() {
+  const buttons = document.querySelectorAll(".importanceLevel");
+  const defaultBtn = buttons[1]; // Medium
+  defaultBtn.classList.add("active");
+  defaultBtn.style.backgroundColor = "orange";
+  defaultBtn.style.color = "white"; // Schriftfarbe beim Standardwert
+  task.priority = "Medium";
+}
+
+function setDateMin() {
+  const dateInput = document.getElementById("DueDate");
+  if (!dateInput) return;
+
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  const dd = String(today.getDate()).padStart(2, "0");
+
+  dateInput.min = `${yyyy}-${mm}-${dd}`;
+  dateInput.value = "";
+}
---
+++ assets/templates/AddTaskTemplate.js
@@ -0,0 +1,106 @@
+function createTaskTemplate(taskName, taskDescription) {
+  return `
+         <h1>Add task</h1>
+      <div class="contentWrapper">
+        <!-- LINKS -->
+        <div class="leftContent">
+          <form id="taskForm">
+            <label class="addTaskLabel" for="taskName">
+              <div class="headlineTextArea">
+                <h2>Title</h2>
+                <p class="star">*</p>
+              </div>
+              <div class="taskNameContainer">
+                <textarea
+                  class="taskName"
+                  type="text"
+                  id="taskName"
+                  name="taskName"
+                  placeholder="Enter a title"
+                  required
+                ></textarea>
+                <p class="requiredField">this field is required</p>
+              </div>
+            </label>
+            <label class="addTaskLabelDescription" for="taskDesc">
+              <h2>Description</h2>
+              <div class="taskDescriptionContainer">
+                <textarea
+                  class="taskDescription"
+                  id="taskDesc"
+                  name="taskDesc"
+                  placeholder="Enter a Description"
+                  required
+                ></textarea>
+                <p class="requiredField">this field is required</p>
+              </div>
+            </label>
+            <label class="addTaskLabelDate" for="DueDate">
+              <div class="headlineTextArea">
+                <h2>Due Date</h2>
+                <p class="star">*</p>
+              </div>
+              <div class="addTaskDateContainer">
+                <input
+                  class="taskDateInput"
+                  type="date"
+                  id="DueDate"
+                  name="DueDate"
+                  placeholder="Select a date"
+                  required
+                />
+                <p class="requiredField">this field is required</p>
+              </div>
+            </label>
+          </form>
+        </div>
+        <div class="divider"></div>
+        <div class="rightContent">
+          <h2>Priority</h2>
+          <div class="priority">
+          <p class="importanceLevel urgent">Urgent</p>
+          <p class="importanceLevel medium">Medium</p>
+          <p class="importanceLevel low">Low</p>
+          </div>
+          <div class="d">
+            <div class="OptionsContainer">
+            <h2 class="ChoiceHeadline">Assigned to</h2>
+           <select class="ChoiceOption borderColorOptions" name="priority" id="assignedTo">
+           <option value="urgent">Select contacts to assign</option>
+           </select>
+           </div>
+           <div class="OptionsContainer">
+           <div class="headlineTextArea">
+           <h2 class="ChoiceHeadline">Category</h2>
+           <p class="star">*</p>
+           </div>
+           <select class="ChoiceOption" name="priority" id="category">
+           <option value="urgent">Select task category</option>
+          </div>
+        </select>
+        </div>
+        <div class="OptionsContainer">
+        <h2 class="ChoiceHeadline">Subtasks</h2>
+        <input class="ChoiceOption inputField" name="ChoiceOption" id="subtaskInput" placeholder="Add new subtask">
+        </select>
+        </div>
+        </div>
+      </div>
+    </div>
+      <div class="buttonRequiredField">
+      <div class="headlineTextArea requiredBottomLeft">
+      <p class="star">*</p>
+      <p class="requiredField">this field is rehhqired</p>
+      </div>
+      <div class="taskButton">
+       <div>
+     <button onclick="clearForm()"  class="clearButton">
+     Clear <span class="cross">&#10006;</span>
+    </button>
+      </div>
+        <button  onclick="createTask()" class="createButton">Create Task
+          <span>&#10003;</span>
+        </button>
+      </div>
+      </div>`;
+}
