7a0039ea82e6e763b176f2039b9e6afa48a038b2
ClaudiaBurkert <burkert.claudia@gmail.com>
2026-02-17 14:53:38 +0100

    check email exists

--- JS/signup.js
+++ JS/signup.js
@@ -10,13 +10,55 @@ async function signUp() {
   let password = document.getElementById('signupPassword');
   let confirm = document.getElementById('signupConfirm');
   if (!checkSignupInputs(name, email, password, confirm)) return;
+  if (await checkEmailExists(email.value)) {
+    // Funktionsaufruf für Fehlermeldung, dass die Email schon existiert.
+    showSignupError(); 
+    return; 
+  }
   document.getElementById('signupButton').disabled = true;
-  await createNewUser(email.value, password.value);
   showToast('You Signed Up successfully');
   setTimeout(function() {
     window.location.href = 'index.html';
   }, 1100);
 }
+
+
+function showSignupError() {
+    document.getElementById('signupEmail').classList.add('InputFieldError');
+    document.getElementById('signupError').textContent = 'This email is already registered.';
+}
+
+
+// 2. 
+async function checkEmailExists(email) {
+  // hier kommt das Objekt an und heißt jetzt loginData
+  let loginData = await getLoginData();
+  // wir schicken das Objekt und die Email an die Checkfunktion
+  return checkEmailData(loginData, email);
+
+}
+
+// 1 das hier hold das die daten und gibt das objekt zurück. 
+async function getLoginData() {
+    let response = await fetch('https://join-6f9cc-default-rtdb.europe-west1.firebasedatabase.app/LoginData.json');
+    let data = await response.json();
+    return data;
+}
+
+// 3. hier holen wir das Objekt und gehen via forschleife durch und prüfen, ob die email schon existiert. und geben true oder false zurück. 
+/** @returns {boolean} */
+function checkEmailData(loginData,email) {
+    let users = Object.values(loginData);
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].email === email) {
+            // dann sollte hier eine Funktion rein, die Fehlermeldung ausgibt
+          return true;
+        }
+    }
+    // wenn die Email neu ist, dann geht es hier weiter. 
+    return false;
+}
+
 
 
 /** @returns {boolean} True if all inputs are valid */
