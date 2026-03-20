0cea0341125efa1745c21b79f5ac20b20b72fb89
Your Name <alex.schoenthaler03@gmail.com>
2026-03-20 19:18:26 +0100

    feat: enhance editPreparation function to include save functionality and improve task assignment handling

--- JS/boardandTask/boardEditTask.js
+++ JS/boardandTask/boardEditTask.js
@@ -1,3 +1,5 @@
+
+let curentTaskID = 0;
 async function editPreparation(taskID) {
   let refTask = TASK[0][`Task${taskID}`];
   document.getElementById("mainContent").innerHTML = createTaskTemplate(`${refTask.title}`,`${refTask.description}`, `${refTask.dueDate}`);
@@ -6,20 +8,50 @@ async function editPreparation(taskID) {
   setupLiveValidation();
   setupPriorityButtons();
   setDefaultPriority(`.priorityButton.${refTask.priority.toLowerCase()}`);
+  task.priority = refTask.priority;
   setupDueDateInput();
   await loadContacts();
+  prepareAssignedToEditTask(refTask);
+  selectCategory(refTask.category);
+  prepareSubTasksEditTask(refTask);
+
+  document.querySelector('.taskButton').remove();
+  document.querySelector('.mainTitle').remove();
+  
+  createSaveDataEditTaskButton(taskID);
+
+}
+
+function prepareAssignedToEditTask(refTask) {
   task.assignedTo = [];
   for (let index = 0; index < refTask.assignedTo.length; index++) {
     task.assignedTo.push(`${refTask.assignedTo[index]}`);
     
   }
   renderSelectedContactsBelowInput();
-  selectCategory(refTask.category);
+}
+
+function prepareSubTasksEditTask(refTask){
+if(refTask.subTasks != [] && refTask.subTasks != undefined && refTask.subTasks != null && refTask.subTasks.length != 0){
   task.subTasks = [];
    for (let index = 0; index < refTask.subTasks.length; index++) {
     task.subTasks.push(`${refTask.subTasks[index]}`);
-    
+    renderSubTasks();
+   }
   }
   
-  renderSubTasks();
+}
+
+function createSaveDataEditTaskButton(taskID) {
+  curentTaskID = taskID;
+  let refsaveButtonEditTask = document.createElement('div');
+  refsaveButtonEditTask.className = "ButtonBlueFilled";
+  refsaveButtonEditTask.innerHTML =`OK <img src="./assets/img/check-2.svg" alt="OK">`;
+  refsaveButtonEditTask.onclick = saveDataEditTask;
+  document.querySelector(".buttonRequiredField").appendChild(refsaveButtonEditTask);
+}
+
+async function saveDataEditTask(){
+  let refTaskKey = `Task${curentTaskID}`;
+  await saveTaskToFirebase(task, curentTaskID, refTaskKey)
 }
